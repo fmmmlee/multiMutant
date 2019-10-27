a517c84aa93a60ea5611dff273e7236ce706cd2b
Matthew Lee <30479162+fmmmlee@users.noreply.github.com>
2019-10-27 13:29:24 -0700

    wget for pdb, altering docker entrypoint

--- containerized/spawnContainers.sh
+++ containerized/spawnContainers.sh
@@ -4,29 +4,28 @@
 # Fall 2019
 # JagResearch
 # MultiMutant
-# Spawns docker containers 
+# Spawns docker containers - requires superuser
 
 # SCRIPT ARGS:
 # pdbID
 # chainID
-# resNumRange (X:Y)
+# resNumRange (X:Y)(inclusive)
 # optional - energy minimization
 
 # amino acids
 declare -a amAcids=("A" "R" "N" "D" "C" "Q" "E" "G" "H" "I" "L" "K"
 					"M" "F" "P" "S" "T" "W" "Y" "V")
 
-
-# runs rMutant with the given arguments
+# starts a docker container that runs rMutant with the given arguments
 # ARGS: pdbID chainID resNum mutTarget (optional)energyMinimization
 # TODO: make an arg for the image name
 runMutant () {
-    sudo docker run multimutanttest $1 $2 $3 $4
+    sudo docker run multimutanttest $1 $2 $3 $4 $5
 }
 
 
 #calls runMutant for each amino acid type
-# ARGS: pdbID chainID resNum
+# ARGS: pdbID chainID resNum (optional)energyMinimization
 allTargets() {
      for j in "${amAcids[@]}"
      do
@@ -34,9 +33,12 @@ allTargets() {
      done
 }
 
-#grabbing range of residues (inclusive)
+#finding range of residues to mutate
 beginning=${3%:*}
 end=${3#*:}
+
+#download specified protein pdb file from rcsb
+wget -O $1 "${1}.pdb" "https://files.rcsb.org/download/${1}.pdb"
 
 #loops over range of residues mutating each into all amino acids
 for ((i = $beginning; i <= end; i++));
