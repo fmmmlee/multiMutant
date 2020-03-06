9053989b3a37acffc331fb7ac959ecbc2ff8bdb9
Matthew Lee <leem53@wwu.edu>
2020-03-05 19:31:26 -0800

    working on getting pipeline running on output of multiMutant

--- regular/invokePipelineOnMutants.py
+++ regular/invokePipelineOnMutants.py
@@ -9,11 +9,12 @@
 
 # pdbID
 # chainID
+# pipelineNumber
 # list of dirs (can be any number of args)
 
 ###### OUTLINE ######
 
-# -> copy pdb file from task 1 multiMutant output subdir to designated pipeline input dir
+# -> copy pdb file from the mutated output subdir to designated pipeline input dir
 # -> run the pipeline
 # -> collect the output file from the output dir and copy it into the original multiMutant output subdir
 # -> clear contents of input/output dirs
@@ -22,8 +23,32 @@
 
 ###### SCRIPT ######
 
+
+import os
+from shutil import copyfile
+
 var pdbID = sys.argv[1]
 var chainID = sys.argv[2]
+var pipelineNum = sys.argv[3]
 
-for i in range (3,len(sys.argv)):
+# for each directory specified in invocation:
+for i in range (4,len(sys.argv)):
+	# -> copy pdb file from the mutated output subdir to designated pipeline input dir
+	var src = os.getcwd() + "./" + sys.argv[i] + "/"
+	var dest = os.getcwd() + "./pipeline/instance_" + sys.argv[3] + "/rMutant-pipeline/WT_C/data/" + sys.argv[1] + "." + sys.argv[2] + ".cur.in/" + sys.argv[1] + ".pdb"
+	var output = os.getcwd() + "./pipeline/instance_" + sys.argv[3] + "/rMutant-pipeline/WT_C/data/" + sys.argv[1] + "." + sys.argv[2] + ".cur.out"
+
+	copyfile(src + "/" + sys.argv[i] + "_em.pdb", dest) 
+
+	# -> run the pipeline
+	subprocess.check_call(["./invokePipeline_WT.sh", sys.argv[1], sys.argv[2]], shell=true)
+	
+	# -> collect the output file from the output dir and copy it into the original multiMutant output subdir
+	for somefile in os.listdir(output):
+		if somefile endswith(".pdb"):
+			copyfile(output + "/" + somefile, src + "/" + somefile)
+			#purge this dir
+	# -> clear contents of input/output dirs
+	#filedelete(dest)
+
 	
--- regular/schedulePipelineTasks.sh
+++ regular/schedulePipelineTasks.sh
@@ -30,29 +30,3 @@ target = $2
 # choose indices based on # of pipelines and size of directories arr
 
 # start invokePipelineOnMutants.py as another process for each set of dirs
-
-while ${#directories[@]} > progress;
-do
-	for progress < target;
-	do
-		#copy pipeline into subdirectory
-		cp $1 directories[progress]
-		
-		#cd into subdirectory
-		cd directories[progress]
-
-		#copy input files into pipeline input folder
-		cp *_em.pdb WT_C/data/$3.cur.in
-		#start pipeline in separate process
-		./invokePipeline_WT.sh #todo
-
-		#then in that process copy output to top level directory and delete pipeline
-
-		#return to parent dir
-		cd ..
-	done
-
-	target *= 2
-
-	#wait 1 minute
-done
